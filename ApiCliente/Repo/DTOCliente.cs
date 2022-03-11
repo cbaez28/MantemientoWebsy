1c0f0ff9abc1cb5dd7cdcfd360663144e471c97b
cbaez28 <51768405+cbaez28@users.noreply.github.com>
2022-03-11 13:45:22 -0800

    Add files via upload

---
+++ ApiCliente/Repo/DTOCliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCliente
+{
+    public class DTOCliente
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+
+        public string OldCorreo { get; set; }
+    }
+}
---
+++ ApiCliente/Repo/IClientes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCliente
+{
+    public  interface IClientes
+    {
+        List<DTOCliente> GetClientes();
+        DTOCliente GetCliente(string  Correo);
+        bool PostCliente(TblCliente clt);
+        bool PutCliente(TblCliente clt, string OldCorreo);
+        bool DeleteCleinte(String  Correo);
+    }
+}
