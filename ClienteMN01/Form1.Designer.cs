3cde2efb0f42c47a5b9821d1b555c86211bfa416
cbaez28 <51768405+cbaez28@users.noreply.github.com>
2022-03-13 10:35:21 -0700

    Add files via upload

---
+++ ClienteMN01/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace ClienteMN01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgwClientes = new System.Windows.Forms.DataGridView();
+            this.nombreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellidoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.correoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dsClientes = new ClienteMN01.DsClientes();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwClientes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgwClientes
+            // 
+            this.dgwClientes.AutoGenerateColumns = false;
+            this.dgwClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwClientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nombreDataGridViewTextBoxColumn,
+            this.apellidoDataGridViewTextBoxColumn,
+            this.correoDataGridViewTextBoxColumn});
+            this.dgwClientes.DataMember = "Clientas";
+            this.dgwClientes.DataSource = this.dsClientes;
+            this.dgwClientes.Location = new System.Drawing.Point(12, 37);
+            this.dgwClientes.Name = "dgwClientes";
+            this.dgwClientes.ReadOnly = true;
+            this.dgwClientes.RowHeadersWidth = 51;
+            this.dgwClientes.RowTemplate.Height = 29;
+            this.dgwClientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgwClientes.Size = new System.Drawing.Size(667, 183);
+            this.dgwClientes.TabIndex = 0;
+            this.dgwClientes.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgwClientes_CellClick);
+            // 
+            // nombreDataGridViewTextBoxColumn
+            // 
+            this.nombreDataGridViewTextBoxColumn.DataPropertyName = "Nombre";
+            this.nombreDataGridViewTextBoxColumn.HeaderText = "Nombre";
+            this.nombreDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nombreDataGridViewTextBoxColumn.Name = "nombreDataGridViewTextBoxColumn";
+            this.nombreDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nombreDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // apellidoDataGridViewTextBoxColumn
+            // 
+            this.apellidoDataGridViewTextBoxColumn.DataPropertyName = "Apellido";
+            this.apellidoDataGridViewTextBoxColumn.HeaderText = "Apellido";
+            this.apellidoDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.apellidoDataGridViewTextBoxColumn.Name = "apellidoDataGridViewTextBoxColumn";
+            this.apellidoDataGridViewTextBoxColumn.ReadOnly = true;
+            this.apellidoDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // correoDataGridViewTextBoxColumn
+            // 
+            this.correoDataGridViewTextBoxColumn.DataPropertyName = "Correo";
+            this.correoDataGridViewTextBoxColumn.HeaderText = "Correo";
+            this.correoDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.correoDataGridViewTextBoxColumn.Name = "correoDataGridViewTextBoxColumn";
+            this.correoDataGridViewTextBoxColumn.ReadOnly = true;
+            this.correoDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // dsClientes
+            // 
+            this.dsClientes.DataSetName = "DsClientes";
+            this.dsClientes.Namespace = "http://tempuri.org/DsClientes.xsd";
+            this.dsClientes.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(166, 259);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(94, 29);
+            this.btnInsertar.TabIndex = 1;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(266, 259);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(94, 29);
+            this.btnActualizar.TabIndex = 2;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(66, 259);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(94, 29);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(366, 259);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(94, 29);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(65, 226);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(185, 27);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(266, 226);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(185, 27);
+            this.txtApellido.TabIndex = 6;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(471, 226);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(185, 27);
+            this.txtCorreo.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 299);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.dgwClientes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgwClientes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwClientes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombreDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellidoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn correoDataGridViewTextBoxColumn;
+        private DsClientes dsClientes;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtCorreo;
+    }
+}
+
---
+++ ClienteMN01/Form1.cs
@@ -0,0 +1,229 @@
+﻿using ApiClientes;
+using ApiClientes.Manager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienteMN01
+{
+    public partial class Form1 : Form
+    {
+        private IClienteHelper _Helper;
+        DataRow _mCurrentClt;
+        public Form1(IClienteHelper hp)
+        {
+            InitializeComponent();
+
+            _Helper = hp;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dsClientes.Tables[0].Clear();
+
+                string strUrl = _Helper.GetUrl("ClienteMN01", "GET");
+
+                if (!string.IsNullOrEmpty(txtCorreo.Text)) strUrl += "/ " + txtCorreo.Text.Trim();
+
+                var _Result = _Helper.Invoke<List<DTOCliente>>(strUrl, "GET");
+
+
+                if (_Result.Success && _Result.DataFound)
+                {
+                    foreach (DTOCliente clt in _Result._Result)
+                    {
+                        DataRow newRow = dsClientes.Tables[0].NewRow();
+                        newRow[0] = clt.Nombre;
+                        newRow[1] = clt.Apellido;
+                        newRow[2] = clt.Correo;
+
+                        dsClientes.Tables[0].Rows.Add(newRow);
+                    }
+                }
+                dgwClientes.Refresh();
+                ClearTxt();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (string.IsNullOrEmpty(txtNombre.Text))
+                {
+                    txtNombre.Focus();
+                    throw new ApplicationException("El nombre es requerido");
+                }
+
+                if (string.IsNullOrEmpty(txtApellido.Text))
+                {
+                    txtApellido.Focus();
+                    throw new ApplicationException("El apellido es requerido");
+                }
+
+                if (string.IsNullOrEmpty(txtCorreo.Text))
+                {
+                    txtCorreo.Focus();
+                    throw new ApplicationException("El crreo es requerido");
+                }
+
+                string strUrl = _Helper.GetUrl("ClienteMN01", "GET");
+                strUrl += "/ " + txtCorreo.Text.Trim();
+
+                var _Result = _Helper.Invoke<List<DTOCliente>>(strUrl, "GET");
+                if (_Result.Success && _Result.DataFound) throw new ApplicationException("Correo Ya Exite");
+
+                strUrl = _Helper.GetUrl("ClienteMN01", "POST");
+
+                DTOCliente newClt = new DTOCliente()
+                {
+                    Nombre = txtNombre.Text.Trim(),
+                    Apellido = txtApellido.Text.Trim(),
+                    Correo = txtCorreo.Text.Trim()
+                };
+
+                var _Result2 = _Helper.Invoke<bool>(strUrl, "POST", newClt);
+                DataRow newRow = dsClientes.Tables[0].NewRow();
+                newRow[0] = newClt.Nombre;
+                newRow[1] = newClt.Apellido;
+                newRow[2] = newClt.Correo;
+
+                dsClientes.Tables[0].Rows.Add(newRow);
+                dgwClientes.Refresh();
+                ClearTxt();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_mCurrentClt == null || _mCurrentClt.Table.Rows.Count <= 0)
+                {
+                    throw new ApplicationException("Debe de seleccionar un cliente");
+                }
+
+                if (string.IsNullOrEmpty(txtNombre.Text))
+                {
+                    txtNombre.Focus();
+                    throw new ApplicationException("El nombre es requerido");
+                }
+
+                if (string.IsNullOrEmpty(txtApellido.Text))
+                {
+                    txtApellido.Focus();
+                    throw new ApplicationException("El apellido es requerido");
+                }
+
+                if (string.IsNullOrEmpty(txtCorreo.Text))
+                {
+                    txtCorreo.Focus();
+                    throw new ApplicationException("El crreo es requerido");
+                }
+
+                string strUrl;
+
+                if (_mCurrentClt["Correo"].ToString().Trim().ToUpper() != txtCorreo.Text.Trim().ToUpper())
+                {
+                    strUrl = _Helper.GetUrl("ClienteMN01", "GET");
+                    strUrl += "/ " + txtCorreo.Text.Trim();
+
+                    var _Result = _Helper.Invoke<List<DTOCliente>>(strUrl, "GET");
+                    if (_Result.Success && _Result.DataFound) throw new ApplicationException("Correo Ya Exite");
+
+                }
+
+                strUrl = _Helper.GetUrl("ClienteMN01", "PUT");
+
+                DTOCliente newClt = new DTOCliente()
+                {
+                    Nombre = txtNombre.Text.Trim(),
+                    Apellido = txtApellido.Text.Trim(),
+                    Correo = txtCorreo.Text.Trim(),
+                    OldCorreo = _mCurrentClt["Correo"].ToString().Trim()
+                };
+
+                var _Result2 = _Helper.Invoke<bool>(strUrl, "PUT", newClt);
+
+                DataRow Row = dsClientes.Tables[0].Select(" Correo = '" + _mCurrentClt["Correo"] + "'")[0];
+                Row[0] = newClt.Nombre;
+                Row[1] = newClt.Apellido;
+                Row[2] = newClt.Correo;
+
+                dgwClientes.Refresh();
+                ClearTxt();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_mCurrentClt == null || _mCurrentClt.Table.Rows.Count <= 0)
+                {
+                    throw new ApplicationException("Debe de seleccionar un cliente");
+                }
+
+                string strUrl = _Helper.GetUrl("ClienteMN01", "DELETE");
+                strUrl += "/ " + txtCorreo.Text.Trim();
+                var _Result2 = _Helper.Invoke<bool>(strUrl, "DELETE");
+
+
+                dsClientes.Tables[0].Rows.Remove(_mCurrentClt);
+                dgwClientes.Refresh();
+                ClearTxt();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void ClearTxt() 
+        {
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtCorreo.Clear();
+        }
+
+        private void dgwClientes_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string Correo = dgwClientes.CurrentRow.Cells[2].Value.ToString().Trim();
+            if (!string.IsNullOrEmpty(Correo))
+            {
+                _mCurrentClt = dsClientes.Tables[0].Select("Correo = '" + Correo + "'")[0];
+
+                if (_mCurrentClt.Table.Rows.Count > 0)
+                {
+                    txtNombre.Text = _mCurrentClt[0].ToString();
+                    txtApellido.Text = _mCurrentClt[1].ToString();
+                    txtCorreo.Text = _mCurrentClt[2].ToString();
+                }
+            }
+        }
+    }
+}
